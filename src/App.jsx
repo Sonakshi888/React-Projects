70d10e076bb85e0c29c6cb7df37af371d8678dab
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-24 17:13:02 +0530

    Update : History of moves displayed on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 import './styles/root.scss';
 import { calculateWinner } from './helpers';
 
@@ -10,22 +11,23 @@ const App = () => {
     isXNext: true,
   }]);
   const [currentMove, setCurrentMove] = useState(0);
-  const current = history[currentMove]; //setting current with the object history (key = currentMove)
-  const [isXNext, setIsXNext] = useState(false);
-  const winner = calculateWinner(current.board);
+  const current = history[currentMove]; //setting current with the object history (key = currentMove)(current = history[0] in first go)
+  
+  const winner = calculateWinner(current.board);  //history[0].board
+  
   const message = winner
     ? `Winner is ${winner}`
     : `Next player is ${current.isXNext ? 'X' : 'O'}`;
-    
+
   /** function to handle click on each sqaure */
   //position is the position of clicked square, pos is the position of iterated square
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
-      //if square value already set then quit the function
+      //if square value already set or winner is assigned then quit the function
       return;
     }
     setHistory(prev => {
-      const last = prev[prev.length - 1];
+      const last = prev[prev.length - 1]; //prev[8] in first go
 
       //prev is the old values of board
       const newBoard = last.board.map((square, pos) => {
@@ -39,11 +41,18 @@ const App = () => {
     setCurrentMove(prev => prev + 1);
   };
 
+  /** function to execute when the go to move number is clicked to show the respective move */
+  const moveTo = (move) => {
+    console.log(move);
+    setCurrentMove(move);
+  }
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 };
