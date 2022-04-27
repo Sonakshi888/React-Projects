806831dcc08a2337010afbfdc4a34e9ad75fe31d
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-27 23:03:59 +0530

    App styled

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -4,13 +4,31 @@ const StatusMessage = ({ winner, current }) => {
   //every method returns true obly if all the el in the array returns true other wise false
   const noMovesLeft = current.board.every(el => el !== null); //it will return true when there will be no space left
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && 'X and O tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is{' '}
+          <span className={current.isNext ? 'text-green' : 'text-orange'}>
+            {' '}
+            {current.isXNext ? 'X' : 'O'}{' '}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className='text-green'>X </span>and
+          <span className='text-orange'> Y</span> tied!
+        </>
+      )}
+    </div>
   );
 };
 
