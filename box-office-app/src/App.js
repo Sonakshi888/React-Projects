aa9df751ff277b0fdaac78d6d41d130fe733c83a
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-28 17:37:22 +0530

    Add : Pages added

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page!!
-      </Route>
-      <Route exact path="/starred">
-        This is starred page!!
-      </Route>
-      <Route>This is 404 page</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>This is 404 page</Route>
+      </Switch>
+    </div>
   );
 }
 
