3727af4b6851ddbf57da84d130ab5cc7c933c188
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-28 17:53:06 +0530

    Add : app layout management and title added

--- box-office-app/src/Pages/Starred.jsx
+++ box-office-app/src/Pages/Starred.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <div>This is Starred Page</div>;
+  return <MainPageLayout>This is starred page!!</MainPageLayout>;
 };
 
 export default Starred;
