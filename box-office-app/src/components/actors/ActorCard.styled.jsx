01f492c6b75ff87c755b4a991fd6029e5c7c1423
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-29 18:25:23 +0530

    Add : styled components added

---
+++ box-office-app/src/components/actors/ActorCard.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { SearchCard } from '../styled';
+
+//extending the style of SearchCard
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
