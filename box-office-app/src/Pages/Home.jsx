5262e8e987cbc31b02a2a4184405503816aefb98
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-04-29 17:10:14 +0530

    Add : created cards for api data

--- box-office-app/src/Pages/Home.jsx
+++ box-office-app/src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actors/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/shows/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -31,19 +33,12 @@ const Home = () => {
       return <div>No results</div>;
     }
 
-    if (results && results.length > 0) /** checking if results are bringing shows or persons */{
-      return results[0].show ? (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
+    if (results && results.length > 0) {
+      /** checking if results are bringing shows or persons */ return results[0]
+        .show ? (
+        <ShowGrid data={results} />
       ) : (
-        <div>
-          {results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ))}
-        </div>
+        <ActorGrid data={results} />
       );
     }
 
