22b17e212a28f69be929ca3bfc0b58b7133ede1d
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-05-02 17:17:50 +0530

    Created custom hook for shows

---
+++ box-office-app/src/misc/custom-hooks.jsx
@@ -0,0 +1,32 @@
+import { useReducer, useEffect } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+    default: {
+      return prevState;
+    }
+  }
+}
+
+function usePersistentReducer(reducer, initialState, key) {
+  // useReducer can have 3 parameters last one being the function that sets the initial value for the state
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  usePersistentReducer(showsReducer, [], key);
+}
