9692853f064cd41e6b2b6f39c9b33b9b8510c086
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-05-16 17:10:58 +0530

    Add : User profile data management

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Container, Loader } from "rsuite";
 import { Route, Redirect } from "react-router";
+import { useProfile } from "../context/profile.context";
 
 const PrivateRoute = ({ children, ...routeProps }) => {
-  const profile = false;
+  const { profile, isLoading } = useProfile();
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="low" />
+      </Container>
+    );
+  }
 
-  if (!profile) {
+  //   if user is not logged in
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
 
