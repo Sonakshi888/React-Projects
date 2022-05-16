743187a1fb033e54f4fec50a3dfbca428fe7e45f
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-05-16 13:26:51 +0530

    Chat App project initialized and sign up page added

---
+++ src/misc/firebase.js
@@ -0,0 +1,20 @@
+// import firebase from "firebase/app";
+// import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore';
+
+const config = {
+  apiKey: "AIzaSyDV1A-i-FJmDx1Gz-t9eqZAFojlSTtQ93M",
+  authDomain: "chat-web-app-7176f.firebaseapp.com",
+  databaseURL: "https://chat-web-app-7176f-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-7176f",
+  storageBucket: "chat-web-app-7176f.appspot.com",
+  messagingSenderId: "189116362691",
+  appId: "1:189116362691:web:58440f3542c4a9c5241776",
+};
+
+const app =firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
