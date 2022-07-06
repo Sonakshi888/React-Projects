bf439d7bebad8c0000d8feeda2fb993e9e7242b4
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-07-06 16:27:56 +0530

    Update: RoomItem updated with last message sent

--- src/components/rooms/RoomItem.jsx
+++ src/components/rooms/RoomItem.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import TimeAgo from "react-timeago";
+import ProfileAvatar from "../ProfileAvatar";
 
 const RoomItem = ({ room }) => {
-  const { createdAt, name } = room;
+  const { createdAt, name, lastMessage } = room;  //getting all the data from room
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">{name}</h3>
-        <TimeAgo className="font-normal text-black-45" date={createdAt} />{" "}
+        <TimeAgo
+          className="font-normal text-black-45"
+          date={
+            lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)
+          }
+        />{" "}
       </div>
       <div className="d-flex align-items-center text-black-70">
-        <span>No messages yet...</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+            <div className="text-disappeat ml-2">
+              <div className="italic">{lastMessage.author.name}</div>
+              <span>{lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No messages yet...</span>
+        )}
       </div>
     </div>
   );
