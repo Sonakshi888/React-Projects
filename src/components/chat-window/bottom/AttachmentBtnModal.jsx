7cdb4bab92e7f3284b0393de9e049bed1ee31df1
chloe sonakshi <chloe.sonakshi@rubicotech.in>
2022-07-14 16:01:28 +0530

    Add: uploaded files to storage and saved in database

---
+++ src/components/chat-window/bottom/AttachmentBtnModal.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { Icon, InputGroup, Modal, Button, Uploader, Alert } from "rsuite";
+import { useModalState } from "../../../misc/custom-hooks";
+import { storage } from "../../../misc/firebase";
+
+const AttachmentBtnModal = ({ afterUpload }) => {
+  const MAX_FILE_SIZE = 1000 * 1024 * 5; //byte * 1024 = 1MB
+
+  const { chatId } = useParams();
+
+  const { isOpen, open, close } = useModalState();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [fileList, setFileList] = useState([]);
+
+  /** function to handle the selection of files */
+  const onChange = (fileArr) => {
+    /** filtering the fileArr by finding the actual size of file using blobFile and comparing it to max file size and then slicing them as we want 5 as maximum number of files being uploaded  */
+    const filtered = fileArr
+      .filter((el) => el.blobFile.size <= MAX_FILE_SIZE)
+      .slice(0, 5);
+    setFileList(filtered);
+  };
+
+  /** function to handle file upload */
+  const onUpload = async () => {
+    try {
+      const uploadPromises = fileList.map((f) => {
+        /** put is used for uploading to the storage */
+        return storage
+          .ref(`/chat/${chatId}`)
+          .child(Date.now() + f.name)
+          .put(f.blobFile, {
+            cacheControl: `public, max-age=${3600 * 24 * 3}`,
+          });
+      }); //map function ended
+
+      const uploadSnapshots = await Promise.all(uploadPromises); //getting snapshot of uploaded promises
+
+      /** mapping the snapshots to get proper needed data */
+      const shapePromises = uploadSnapshots.map(async (snap) => {
+        return {
+          contentType: snap.metadata.contentType,
+          name: snap.metadata.name,
+          url: await snap.ref.getDownloadURL(),
+        };
+      });
+
+      const files = await Promise.all(shapePromises); //files that will be set to database
+
+      await afterUpload(files);
+
+      setIsLoading(false);
+      close();
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <InputGroup.Button onClick={open}>
+        <Icon icon="attachment" />
+      </InputGroup.Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>Upload Files</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Uploader
+            autoUpload={false}
+            action=""
+            fileList={fileList}
+            onChange={onChange}
+            multiple
+            listType="picture-text"
+            className="w-100"
+            disabled={isLoading}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block disabled={isLoading} onClick={onUpload}>
+            Send to chat
+          </Button>
+          <div className="text-right mt-2">
+            <small>*only files less than 5MB are allowed</small>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default AttachmentBtnModal;
--- src/components/chat-window/bottom/index.jsx
+++ src/components/chat-window/bottom/index.jsx
@@ -6,8 +6,9 @@ import firebase from "firebase/compat/app";
 import { useParams } from "react-router";
 import { useProfile } from "../../../context/profile.context";
 import { database } from "../../../misc/firebase";
+import AttachmentBtnModal from "./AttachmentBtnModal";
 
-//function to assemble the messages
+/** function to assemble the messages */
 function assembleMessage(profile, chatId) {
   return {
     roomId: chatId,
@@ -28,12 +29,12 @@ const Bottom = () => {
   const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  // input change hanler
+  /** input change hanler */
   const onInputChange = useCallback((value) => {
     setInput(value);
   }, []);
 
-  // send button handler
+  /**  send button handler */
   const onSendClick = async () => {
     if (input.trim() === "") {
       return;
@@ -42,14 +43,18 @@ const Bottom = () => {
     msgData.text = input; //second, append new message to the already existing msgs
 
     const updates = {}; //an empty object
+
     const messageId = database.ref("messages").push().key; //getting the unique key for new message
     updates[`messages/${messageId}`] = msgData; //setting the object with the message
+
     updates[`rooms/${chatId}/lastMessage`] = {
       //setting the object with room's new data i,e. last message
       ...msgData,
       msgId: messageId,
     };
+
     setIsLoading(true);
+
     // saving message to the realtime firebase database
     try {
       await database.ref().update(updates);
@@ -61,7 +66,7 @@ const Bottom = () => {
     }
   };
 
-  // function to call onsendclick when enter is pressed to send the message
+  /** function to call onsendclick when enter is pressed to send the message */
   const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       ev.preventDefault();
@@ -69,9 +74,45 @@ const Bottom = () => {
     }
   };
 
+  /** function to handle after file upload */
+  const afterUpload = useCallback(
+    async (files) => {
+      setIsLoading(true);
+      const updates = {};
+
+      /** assembling msg for each file from files array */
+      files.forEach((file) => {
+        const msgData = assembleMessage(profile, chatId); //first get the earlier msgs data
+        msgData.file = file;
+
+        const messageId = database.ref("messages").push().key; //getting the unique key for new message
+        updates[`messages/${messageId}`] = msgData; //setting the object with the message
+      });
+
+      /** getting the last message */
+      const lastMsgId = Object.keys(updates).pop();
+
+      updates[`rooms/${chatId}/lastMessage`] = {
+        //setting the object with room's new data i,e. last message
+        ...updates[lastMsgId],
+        msgId: lastMsgId,
+      };
+
+      try {
+        await database.ref().update(updates);
+        setIsLoading(false);
+      } catch (err) {
+        Alert.error(err.message);
+        setIsLoading(false);
+      }
+    },
+    [chatId, profile]
+  );
+
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal afterUpload={afterUpload} />
         <Input
           placeholder="Write a new message here..."
           value={input}
